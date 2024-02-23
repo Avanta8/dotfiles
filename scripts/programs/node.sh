684f46074772b0729d61dc1196e0a776f2015a62
Harry Min <harryhmin@gmail.com>
2024-02-23 23:31:07 +0000

    redirect stderr for command

--- scripts/programs/node.sh
+++ scripts/programs/node.sh
@@ -5,7 +5,7 @@ if [ ! -f "$NVM_DIR/nvm.sh" ]; then
     source "$HOME/.bash_profile"
 fi
 
-if ! command -v node &>/dev/null; then
+if ! command -v node >/dev/null 2>&1; then
     source "$NVM_DIR/nvm.sh"
     nvm install --lts
     nvm use --lts
