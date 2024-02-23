567e7d5c2ae2a316c1da9dc232d1e1432d22491b
Harry Min <harryhmin@gmail.com>
2024-02-23 11:51:42 +0000

    set indent to two spaces for the specified files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,23 @@ vim.api.nvim_create_autocmd("VimLeave", {
   group = vim.api.nvim_create_augroup("RestoreCursorShapeOnExit", { clear = true }),
   command = "set guicursor=a:ver80-blinkwait700-blinkoff400-blinkon250",
 })
+
+-- Set indentation to 2 spaces
+vim.api.nvim_create_autocmd("Filetype", {
+  group = vim.api.nvim_create_augroup("setIndent", { clear = true }),
+  pattern = {
+    "css",
+    "html",
+    "dart",
+    "javascript",
+    "lua",
+    "markdown",
+    "md",
+    "typescript",
+    "scss",
+    "xml",
+    "xhtml",
+    "yaml",
+  },
+  command = "setlocal shiftwidth=2 tabstop=2",
+})
