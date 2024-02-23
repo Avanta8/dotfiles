3eb3e112f588e62718b6e947c94c01af6f45f716
Harry Min <harryhmin@gmail.com>
2024-02-23 11:18:16 +0000

    change esc timeout and regorganize a little

--- .config/blesh/init.sh
+++ .config/blesh/init.sh
@@ -1,9 +1,18 @@
-set -o vi
-ble-bind -m vi_cmap --cursor 0
-ble-bind -m vi_imap --cursor 5
-ble-bind -m vi_nmap --cursor 2
-ble-bind -m vi_omap --cursor 4
-ble-bind -m vi_smap --cursor 2
-ble-bind -m vi_xmap --cursor 2
+# vi mode by default: https://github.com/akinomyoga/ble.sh/wiki/Vi-%28Vim%29-editing-mode#with-blesh-setting-in-blerc
+bleopt default_keymap=vi
 
-ble-bind -m vi_imap -f "C-h" "vi_imap/delete-backward-word"
+# vi mode esc timeout: https://github.com/akinomyoga/ble.sh/wiki/Vi-%28Vim%29-editing-mode#timeout-of-bash-43
+bind 'set keyseq-timeout 1'
+
+# preferences: https://github.com/akinomyoga/ble.sh/wiki/Vi-%28Vim%29-editing-mode#3-possible-preferences
+function blerc/vim-mode-hook {
+	ble-bind -m vi_cmap --cursor 0
+	ble-bind -m vi_imap --cursor 5
+	ble-bind -m vi_nmap --cursor 2
+	ble-bind -m vi_omap --cursor 4
+	ble-bind -m vi_smap --cursor 2
+	ble-bind -m vi_xmap --cursor 2
+
+	ble-bind -m vi_imap -f "C-h" "vi_imap/delete-backward-word"
+}
+blehook/eval-after-load keymap_vi blerc/vim-mode-hook
