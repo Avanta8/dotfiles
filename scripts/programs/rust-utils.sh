b75611e1dc9b2c914bfe32a6c481880a898a981b
Harry Min <harryhmin@gmail.com>
2024-02-23 00:49:41 +0000

    rust utils

---
+++ scripts/programs/rust-utils.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if ! command -v cargo >/dev/null 2>&1; then
+    echo "rust is required"
+    exit 1
+fi
+
+cargo install bat
+cargo install exa
+cargo install ripgrep
+cargo install kondo
+cargo install tlrc
+cargo install lemmeknow
+cargo install tokei
