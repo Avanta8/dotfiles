5bac9dacb7c015a48d917c89ae4344471dd0dcdb
Harry Min <harryhmin@gmail.com>
2024-02-23 11:43:05 +0000

    refactor

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,8 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_augroup("RestoreCursorShapeOnExit", { clear = true })
 vim.api.nvim_create_autocmd("VimLeave", {
-  group = "RestoreCursorShapeOnExit",
+  group = vim.api.nvim_create_augroup("RestoreCursorShapeOnExit", { clear = true }),
   command = "set guicursor=a:ver80-blinkwait700-blinkoff400-blinkon250",
 })
