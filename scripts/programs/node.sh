b5bc4f5be7df6fc0c100bc8197582983fc1cabbb
Harry Min <harryhmin@gmail.com>
2024-02-23 22:33:06 +0000

    fix node install

--- scripts/programs/node.sh
+++ scripts/programs/node.sh
@@ -2,9 +2,11 @@
 
 if [ ! -f "$NVM_DIR/nvm.sh" ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    source "$HOME/.bash_profile"
 fi
 
 if ! command -v node &>/dev/null; then
     source "$NVM_DIR/nvm.sh"
     nvm install --lts
+    nvm use --lts
 fi
