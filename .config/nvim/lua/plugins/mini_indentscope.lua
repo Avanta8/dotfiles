d545d52db52531a123a1daa4acc985a7c354bbc6
Harry Min <harryhmin@gmail.com>
2024-02-23 19:42:47 +0000

    remove mini.indentscope animation

---
+++ .config/nvim/lua/plugins/mini_indentscope.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      draw = {
+        delay = 0,
+        animation = require("mini.indentscope").gen_animation.none(),
+      },
+      options = {
+        -- indent_at_cursor = false,
+      },
+    },
+  },
+}
