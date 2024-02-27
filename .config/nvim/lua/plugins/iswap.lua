1492ce93fea1863109196f74d03e8e62546c1268
Harry Min <harryhmin@gmail.com>
2024-02-27 21:47:49 +0000

    iswap

---
+++ .config/nvim/lua/plugins/iswap.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        -- ["gs"] = { name = "+surround" },
+        ["<leader>i"] = { name = "+iswap" },
+      },
+    },
+  },
+  {
+    "mizlan/iswap.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "<leader>is", "<cmd>ISwap<cr>", desc = "ISwap" },
+      { "<leader>iw", "<cmd>ISwapWith<cr>" },
+      { "<leader>in", "<cmd>ISwapNode<cr>" },
+
+      { "<leader>im", "<cmd>IMove<cr>" },
+      { "<leader>ie", "<cmd>IMoveWith<cr>" },
+      { "<leader>ij", "<cmd>IMoveNode<cr>" },
+    },
+  },
+}
