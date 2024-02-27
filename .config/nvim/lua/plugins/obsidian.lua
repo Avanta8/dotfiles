a402e2444a497ab8b86d058e12ddea362f25a429
Harry Min <harryhmin@gmail.com>
2024-02-27 21:48:30 +0000

    obsidian

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "notes",
+          path = "~/.notes",
+        },
+      },
+    },
+  },
+}
