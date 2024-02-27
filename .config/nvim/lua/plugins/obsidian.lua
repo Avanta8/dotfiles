f382998e5f889bd983e4f3f1839b91edeeced4a0
Harry Min <harryhmin@gmail.com>
2024-02-27 23:36:26 +0000

    update keymaps

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -6,6 +6,22 @@ return {
       "nvim-lua/plenary.nvim",
     },
     opts = {
+      mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- Toggle check-boxes.
+        ["<leader>lh"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true, desc = "Toggle Checkbox" },
+        },
+      },
       workspaces = {
         {
           name = "notes",
