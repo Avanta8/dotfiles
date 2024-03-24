4f2b9ce79516437f4257b40cee22864bf99d64df
Harry Min <harryhmin@gmail.com>
2024-03-24 13:13:00 +0000

    Hack to fix pyenv

--- scripts/programs/pyenv.sh
+++ scripts/programs/pyenv.sh
@@ -1,6 +1,8 @@
 if [ ! -d "$HOME/.pyenv/bin" ]; then
     curl https://pyenv.run | sh
-    source "$HOME/.bash_profile"
+    export PYENV_ROOT="$HOME/.pyenv"
+    [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
 fi
 
 yes n | pyenv install 3
