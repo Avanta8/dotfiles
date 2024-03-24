7ae2ccdcb0dcf54f6d00681b4910cc36db505cb1
Harry Min <harryhmin@gmail.com>
2024-03-24 12:40:29 +0000

    fix node

--- scripts/programs/node.sh
+++ scripts/programs/node.sh
@@ -2,6 +2,7 @@
 
 if [ ! -f "$NVM_DIR/nvm.sh" ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"  # The line below doesn't seem to export this so I need to do it here
     source "$HOME/.bash_profile"
 fi
 
